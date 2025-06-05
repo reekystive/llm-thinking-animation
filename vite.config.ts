135c8f8509fd396290803e04a818ef27bf90a9d3
Mingxuan Wang <wangmingxuan.io@bytedance.com>
2025-06-05 21:11:34 +0800

    chore: 🔧 add banner insertion to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { bannerContent } from './scripts/banner.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
+    rollupOptions: { output: { banner: bannerContent } },
   },
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {},
