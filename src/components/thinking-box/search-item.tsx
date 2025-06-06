eab0a53af062444fcbaf8437819d29df6c811086
Mingxuan Wang <wangmingxuan.io@bytedance.com>
2025-06-06 19:59:17 +0800

    feat: ✨ add search icon into search-item component and refine layout for better visual appeal

--- src/components/thinking-box/search-item.tsx
+++ src/components/thinking-box/search-item.tsx
@@ -1,5 +1,6 @@
 import { Website } from '#src/mocks/thinking-interface.ts';
 import { cn } from '#src/utils/cn.ts';
+import { SearchIcon } from 'lucide-react';
 import { FC } from 'react';
 
 export const SearchItem: FC<{ data: Website; className?: string }> = ({ data, className }) => {
@@ -7,17 +8,24 @@ export const SearchItem: FC<{ data: Website; className?: string }> = ({ data, cl
   return (
     <div
       className={cn(
-        'flex flex-row items-center gap-1 rounded-full border-[1px] border-gray-200 bg-gray-50 py-0.5 pr-2 text-xs leading-[1.2em] dark:border-gray-700 dark:bg-gray-900',
-        !faviconUrl && 'pl-2',
-        faviconUrl && 'pl-0.5',
+        'flex flex-row items-center rounded-full border-[1px] border-gray-200 bg-gray-50 py-0.5 pr-1.5 pl-0.5 text-xs leading-[1.2em] dark:border-gray-700 dark:bg-gray-900',
+        faviconUrl && 'gap-[3px]',
+        !faviconUrl && 'gap-[1px]',
         className
       )}
     >
-      {faviconUrl && (
-        <div className="flex aspect-square h-[1.2em] items-center justify-center overflow-clip rounded-full bg-gray-200 p-[1px] dark:bg-gray-600">
+      <div
+        className={cn(
+          'flex aspect-square h-[1.2em] items-center justify-center overflow-clip rounded-full p-[1px]',
+          faviconUrl && 'bg-gray-200 dark:bg-gray-600'
+        )}
+      >
+        {faviconUrl ? (
           <img src={faviconUrl} alt={title} className="block h-full w-full overflow-clip rounded-full object-cover" />
-        </div>
-      )}
+        ) : (
+          <SearchIcon className="block h-full w-full overflow-clip rounded-full p-[0.5px]" />
+        )}
+      </div>
       <div>{title}</div>
     </div>
   );
