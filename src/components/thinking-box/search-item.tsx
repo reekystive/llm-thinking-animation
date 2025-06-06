13bd053ac074c2e994ba10f3665945db2a7be677
Mingxuan Wang <wangmingxuan.io@bytedance.com>
2025-06-06 18:48:15 +0800

    style: 🎨 refine layout and styling of SearchItem component for improved visual consistency

--- src/components/thinking-box/search-item.tsx
+++ src/components/thinking-box/search-item.tsx
@@ -7,14 +7,15 @@ export const SearchItem: FC<{ data: Website; className?: string }> = ({ data, cl
   return (
     <div
       className={cn(
-        'flex flex-row items-center gap-1 rounded-full border-[1px] border-gray-200 bg-gray-50 px-2 py-0.5 pl-0.5 text-xs dark:border-gray-700 dark:bg-gray-900',
+        'flex flex-row items-center gap-1 rounded-full border-[1px] border-gray-200 bg-gray-50 py-0.5 pr-2 text-xs leading-[1.2em] dark:border-gray-700 dark:bg-gray-900',
         !faviconUrl && 'pl-2',
+        faviconUrl && 'pl-0.5',
         className
       )}
     >
       {faviconUrl && (
-        <div className="flex h-4 w-4 items-center justify-center overflow-clip rounded-full bg-gray-50">
-          <img src={faviconUrl} alt={title} className="block h-3 w-3" />
+        <div className="flex aspect-square h-[1.2em] items-center justify-center overflow-clip rounded-full bg-gray-200 p-[1px] dark:bg-gray-600">
+          <img src={faviconUrl} alt={title} className="block h-full w-full overflow-clip rounded-full object-cover" />
         </div>
       )}
       <div>{title}</div>
